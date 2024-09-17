fee770f108c4f0069c0ae802cd4a2f22ad2989c5
raf <rraf@tuta.io>
2024-09-17 10:17:47 +0100

    chore: writing gnome module

--- default.nix
+++ default.nix
@@ -1,5 +1,31 @@
-{ ... }:
+{ pkgs, ... }:
 
+let
+  USER = "raf";
+in
 {
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+  };
 
+  environment.gnome.excludePackages =
+    (with pkgs; [
+      gnome-tour
+      gnome-connections
+    ])
+    ++ (with pkgs.gnome; [
+      epiphany
+      geary
+      evince
+    ]);
+
+  environment.systemPackages = with pkgs.gnomeExtensions; [ pop-shell ];
+
+  home-manager.users.${USER} = {
+    home = {
+      dconf.enable = true;
+    };
+  };
 }
